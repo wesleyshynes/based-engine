c9faabd8f72b61116a7a19443164ac082c116eb4
Wesley Shynes <wesleyshynes@gmail.com>
2021-06-19 14:33:23 -0400

    generic settings

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import './assets/style.scss';
-import { startGame } from './games/blasty-man/startGame';
+import { startGame } from './games/walk-the-human/startGame';
+// import { startGame } from './games/blasty-man/startGame';
 
 const startBtn = document.getElementById('start-game')
 const gameDesc = document.getElementById('game-description')
 const audioP: any = document.getElementById('audio-b')
 // const fakeSound = new Audio()
-// const fakeSound = new Audio('https://www.dizzisternberg.co.uk/music/Picts.mp3')
+// const fakeSound = new Audio('https://www.someaudio.com/something.mp3')
 if(startBtn && audioP && audioP) {
   startBtn.addEventListener('click', () => {
     // fakeSound.muted = false
